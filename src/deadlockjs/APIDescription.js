df9b728ddd8f0f3cf39710e30759003c755fc706
7PH <b.raymond@protonmail.com>
2018-02-15 18:42:50 +0100

    Removed javascript files

--- src/deadlockjs/APIDescription.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var APIRouteType;
-(function (APIRouteType) {
-    APIRouteType[APIRouteType["ENDPOINT"] = 0] = "ENDPOINT";
-    APIRouteType[APIRouteType["DIRECTORY"] = 1] = "DIRECTORY";
-})(APIRouteType = exports.APIRouteType || (exports.APIRouteType = {}));
