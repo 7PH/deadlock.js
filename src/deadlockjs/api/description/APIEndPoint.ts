ba20cd2df5b4207c3cec865fc5c24a0db6706e99
7PH <b.raymond@protonmail.com>
2018-02-21 00:14:07 +0100

    updated APIEndPoint, added cache configuration

--- src/deadlockjs/api/description/APIEndPoint.ts
+++ src/deadlockjs/api/description/APIEndPoint.ts
@@ -1,17 +1,30 @@
 import {RequestHandler} from "express";
 import * as iof from "io-filter";
+import * as express from "express";
 import {APIRoute} from "./APIRoute";
 
 /** An API end-point is an application entry point */
 export interface APIEndPoint extends APIRoute {
     /** relative path of this end-point */
     path: string;
+
     /** method to use */
     method: 'get' | 'post' | 'put' | 'delete';
+
     /** will handle the request */
     handler: RequestHandler;
-    /** parameters */
+
+    /** if you want to ensure request body is filled with valid data, use a valid MaskFilter here */
     paramFilter?: iof.MaskFilter,
+
+    /** caching @TODO implement */
+    cache?: {
+        /** identifier function (by default, IP address) */
+        identifier?: (req: express.Request, res: express.Response) => string,
+        /** expire time */
+        timeout: number;
+    };
+
     /** wether this end-point needs database allocation (an instance of mysql) */
     dbConnection: boolean;
 }
