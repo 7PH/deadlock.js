09b8fa8176e48f22ff0980cdbb9102e872c978b6
7PH <b.raymond@protonmail.com>
2018-02-22 00:20:58 +0100

    Added the appSecret into APIDescription

--- src/deadlockjs/api/description/APIDescription.ts
+++ src/deadlockjs/api/description/APIDescription.ts
@@ -1,17 +1,28 @@
 import {MySQLDescription} from "./MySQLDescription";
 import {APIDirectory} from "./APIDirectory";
+import {RateLimiterConfig} from "../wrapper/preprocessor/RateLimiter";
 
 
 
 /** The API description describes the whole API */
 export interface APIDescription {
+    /** app secret */
+    appSecret: string;
+
     /** hostname of the API */
     hostname: string;
+
     /** port */
     port: number;
+
+    /** database */
     db?: {
         mysql?: MySQLDescription;
-    },
+    };
+
+    /** DDoS */
+    rateLimit?: RateLimiterConfig;
+
     /** root directory. as it is an APIDirectory, it can be in a sub-directory, for instance, example.com/api/v0/ */
     root: APIDirectory;
 }
