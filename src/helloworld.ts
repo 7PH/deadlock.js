02fd8dfdc14cf5feb629a0e074e467673a26dfc6
7PH <b.raymond@protonmail.com>
2018-02-23 00:21:20 +0100

    Added example and edited main export file for npm

---
+++ src/helloworld.ts
@@ -0,0 +1,26 @@
+import * as express from "express";
+import {APIDescription} from "./deadlockjs/api/description/APIDescription";
+import {APIRouteType} from "./deadlockjs/api/description/APIRouteType";
+import {DeadLockJS} from "./deadlockjs/DeadLockJS";
+
+
+const api: APIDescription = {
+    appSecret: '1f4600bc0380273f90ed02db217cfbf',
+    workers: 2,
+    port: 3000,
+    root: {
+        kind: APIRouteType.DIRECTORY,
+        path: '/api/v1',
+        routes: [
+            {
+                kind: APIRouteType.END_POINT,
+                path: '/',
+                method: 'get',
+                handler: (req: express.Request, res: express.Response) => { res.json({hello: "world"}); }
+            }
+        ]
+    }
+};
+
+
+DeadLockJS.startApp(api);
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
 
 
-export * from './deadlockjs/DeadLockJS';
+
 export * from './deadlockjs/api/description/APIDescription';
 export * from './deadlockjs/api/description/APIDirectory';
 export * from './deadlockjs/api/description/APIEndPoint';
 export * from './deadlockjs/api/description/APIRoute';
 export * from './deadlockjs/api/description/APIRouteType';
-export * from './deadlockjs/api/description/MySQLDescription';
-export * from './deadlockjs/api/wrapper/RequestWrapper';
+export * from './deadlockjs/api/description/MySQLConfig';
+
+export * from './deadlockjs/api/wrapper/local/RequestLocal';
+export * from './deadlockjs/api/wrapper/preprocessor/DBConnectionCleaner';
+export * from './deadlockjs/api/wrapper/preprocessor/DBConnectionProvider';
+export * from './deadlockjs/api/wrapper/preprocessor/IPreprocessor';
+export * from './deadlockjs/api/wrapper/preprocessor/RateLimiter';
+export * from './deadlockjs/api/wrapper/preprocessor/RequestBodyChecker';
+export * from './deadlockjs/api/wrapper/preprocessor/RequestInitializer';
 export * from './deadlockjs/api/wrapper/IRequestWrapper';
+export * from './deadlockjs/api/wrapper/RequestWrapper';
+
+export * from './deadlockjs/DeadLockJS';
