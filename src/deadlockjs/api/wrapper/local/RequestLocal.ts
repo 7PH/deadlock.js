78be56aab509fa1a754518db059a3b64f5ccb60d
7PH <b.raymond@protonmail.com>
2018-03-13 20:30:45 +0100

    mysql and mongodb not optional on RequestLocal anymore

--- src/deadlockjs/api/wrapper/local/RequestLocal.ts
+++ src/deadlockjs/api/wrapper/local/RequestLocal.ts
@@ -2,8 +2,8 @@ import {PoolConnection} from "mysql";
 import {MongoClient} from "mongodb";
 
 export interface RequestLocal {
-    mysql?: PoolConnection;
-    mongodb?: MongoClient;
+    mysql: PoolConnection;
+    mongodb: MongoClient;
     params: any;
     ip: string;
 }
