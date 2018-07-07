f5489abd7688e7e2ee80c6296db927c3adc6eb55
7PH <b.raymond@protonmail.com>
2018-07-07 13:50:16 +0200

    Improve TimingAttack w/ data serialization

--- src/deadlockjs/api/wrapper/preprocessor/TimingAttack.ts
+++ src/deadlockjs/api/wrapper/preprocessor/TimingAttack.ts
@@ -1,21 +1,30 @@
 import {Preprocessor} from "./Preprocessor";
-import {APIEndPoint} from "../../../../";
+import {APIEndPoint, RequestLocal} from "../../../../";
 import {Request, Response} from "express";
+import {PromiseCaching} from "promise-caching";
 
 export class TimingAttack implements Preprocessor {
 
-    public preprocess(endPoint: APIEndPoint, req: Request, res: Response): Promise<void> {
-        return new Promise<void>((resolve, reject) => {
+    public static get CACHE_DURATION(): number { return 60 * 1000; }
 
-            if (typeof endPoint.timingAttack === 'undefined')
-                return resolve();
+    private cache: PromiseCaching = new PromiseCaching();
 
-            const delay: number = endPoint.timingAttack.minDelay
-                + Math.random()
-                * (endPoint.timingAttack.maxDelay - endPoint.timingAttack.minDelay);
-
-            setTimeout(resolve, delay);
+    public async delay(min: number, max: number, loc: RequestLocal): Promise<void> {
+        let serialized: string = JSON.stringify(loc.requestInfo.params);
+        let expire: number = TimingAttack.CACHE_DURATION;
+        let delayDuration: number = await this.cache.get(serialized, expire, async () => {
+            return min + Math.random() * (max - min);
         });
+
+        return new Promise<void>(resolve => setTimeout(resolve.bind(this), delayDuration));
+    }
+
+    public async preprocess(endPoint: APIEndPoint, req: Request, res: Response): Promise<void> {
+        if (typeof endPoint.timingAttack !== 'undefined') {
+            const min: number = endPoint.timingAttack.minDelay;
+            const max: number = endPoint.timingAttack.minDelay;
+            await this.delay(min, max, res.locals);
+        }
     }
 
 }
